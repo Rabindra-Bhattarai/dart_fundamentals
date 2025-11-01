c55e564a028d00e9241be886baa037e34ad54a8e
Rabindra-Bhattarai <rabindrabhattarai01@gmail.com>
2025-11-01 17:43:14 +0545

    update

---
+++ bin/multiplication.dart
@@ -0,0 +1,6 @@
+void main() {
+  int num = 3;
+  displayMultiplicationTable(num: num);
+}
+
+void displayMultiplicationTable({required int num}) {}
---
+++ bin/type_interface.dart
@@ -0,0 +1,11 @@
+void main() {
+  //Type interface
+  var b = 34.8;
+
+  dynamic c;
+  c = "abbdbd";
+  print(c);
+
+  c = 4;
+  c = true;
+}
