0fe38d84446b7d8fe520ac554b413d12e0e0feec
Rabindra-Bhattarai <rabindrabhattarai01@gmail.com>
2025-11-02 10:15:07 +0545

    sting palindrome updated

--- bin/string_palindrome.dart
+++ bin/string_palindrome.dart
@@ -1,14 +1,27 @@
+// void main() {
+//   String str = "madam";
+//   String reversed = "";
+
+//   for (int i = str.length - 1; i >= 0; i--) {
+//     reversed += str[i];
+//   }
+
+//   if (str == reversed) {
+//     print("$str is a Palindrome string");
+//   } else {
+//     print("$str is not a Palindrome string");
+//   }
+// }
+
+//updated and easy one
+
 void main() {
-  String str = "madam";
-  String reversed = "";
+  String name = "olleh";
+  String backward = name.split('').reversed.join('');
 
-  for (int i = str.length - 1; i >= 0; i--) {
-    reversed += str[i];
-  }
-
-  if (str == reversed) {
-    print("$str is a Palindrome string");
+  if (name == backward) {
+    print("Its is palindrone");
   } else {
-    print("$str is not a Palindrome string");
+    print("It is not");
   }
 }
